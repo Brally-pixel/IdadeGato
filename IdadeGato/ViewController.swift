b5db02521dc85547db6bbed1113f5e711d06a151
Jorge Soares <jhorge.junior@gmail.com>
2022-01-20 06:55:23 -0300

    primeiro app

--- IdadeGato/ViewController.swift
+++ IdadeGato/ViewController.swift
@@ -8,7 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    @IBAction func calcular(_ sender: Any) {
+        
+        let idade = ((Int(campoIdadeGato.text!)! * 12) / 2 ) * 3
+        let nome = campoNomeGato.text!
+        
+        
+        legendaResultado.text = "A idade do " + nome + " é " + String(idade) + " anos"
 
+    
+    }
+ 
+    @IBOutlet weak var campoNomeGato: UITextField!
+
+    
+    @IBOutlet weak var campoIdadeGato: UITextField!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
